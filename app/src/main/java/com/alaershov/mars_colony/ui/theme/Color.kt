4d5f232d85000a5d4bb3331a3c5d449d806d0a51
Alexey Ershov <aershov@whoosh.bike>
2023-07-12 12:49:58 +0700

    Make color theme more Martian

--- app/src/main/java/com/alaershov/mars_colony/ui/theme/Color.kt
+++ app/src/main/java/com/alaershov/mars_colony/ui/theme/Color.kt
@@ -2,10 +2,5 @@ package com.alaershov.mars_colony.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val MarsOrange = Color(0xFFE68A5C)
+val MarsGrey = Color(0xFFf0e7e7)
